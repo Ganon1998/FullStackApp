ee4481a7d3f188f11944dad1c8143c2686864aae
Jordan <jgreene097@gmail.com>
2021-12-12 23:53:59 -0500

    Fixed a image glitch and MAYBE deployment fix

--- server/index.js
+++ server/index.js
@@ -20,6 +20,10 @@ app.use('/posts', postRoutes);
 
 const PORT = process.env.PORT || 5000;
 
+app.get('/', (req, res) => {
+    res.send('App is running :)');
+});
+
 // connect to MongoDB using mongoose
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => app.listen(PORT, () => console.log('Server running')))
